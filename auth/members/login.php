91550be74eaeca58a6a7e5ba82eac0d7c0831eb4
rianinajaa <fathanaprian73@gmail.com>
2025-08-10 12:37:51 +0700

    login

---
+++ auth/members/login.php
@@ -0,0 +1,25 @@
+<?php 
+
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+    <label for="username">username</label>
+    <input type="text" name="username">
+
+     <label for="password">password</label>
+    <input type="text" name="password">
+    </form>
+</body>
+</html>
