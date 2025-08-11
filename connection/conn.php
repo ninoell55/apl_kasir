34b941bbb4020a14b66511139b9c2f21a800736a
rianinajaa <fathanaprian73@gmail.com>
2025-08-11 19:48:03 +0700

    login

--- connection/conn.php
+++ connection/conn.php
@@ -4,9 +4,11 @@ $hostname = 'localhost';
 $username = 'root';
 $password = '';
 $dbname = 'apl_kasir';
+$port = 3309;
+
 
 // Create connection
-$conn = new mysqli($hostname, $username, $password, $dbname);
+$conn = new mysqli($hostname, $username, $password, $dbname, $port);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
