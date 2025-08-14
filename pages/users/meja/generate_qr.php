a5e0033c4982b64235bc5ebf3ccbea7beeb7d167
rianinajaa <fathanaprian73@gmail.com>
2025-08-14 09:31:10 +0700

    update_qrcode

--- pages/users/meja/generate_qr.php
+++ pages/users/meja/generate_qr.php
@@ -1,15 +1,36 @@
 <?php
 require_once "../../../libs/phpqrcode/qrlib.php";
+include "../../../connection/conn.php";
 
-// Base URL halaman menu
+
 $base_url = "http://localhost/apl_kasir/pages/users/menu/menu.php?id_meja=";
 
-// Jumlah meja
-$jumlah_meja = 5; // Sesuaikan jumlah meja
 
-for($i = 1; $i <= $jumlah_meja; $i++) {
-    $link_meja = $base_url . $i;
-    $filename = "qrcode_meja_" . $i . ".png";
-    QRcode::png($link_meja, $filename, QR_ECLEVEL_L, 5);
-    echo "QR Code untuk meja $i dibuat: <img src='$filename'><br>";
+$query = mysqli_query($conn, "SELECT id_meja FROM meja");
+
+while ($row = mysqli_fetch_assoc($query)) {
+    $id_meja = $row['id_meja'];
+    $link_meja = $base_url . $id_meja;
+
+    // Folder penyimpanan QR code
+    $folder = "img/";
+    if (!file_exists($folder)) {
+        mkdir($folder, 0777, true); // buat folder jika belum ada
+    }
+
+    // Nama file QR code
+    $filename = "qrcode_meja_" . $id_meja . ".png";
+    $file_path = $folder . $filename;
+
+    // Generate QR code dan simpan ke file
+    QRcode::png($link_meja, $file_path, QR_ECLEVEL_L, 5);
+
+    // Path relatif untuk simpan di database (agar bisa dipanggil di web)
+    $db_path = "img/" . $filename;
+
+    // Update kolom qrcode di database
+    mysqli_query($conn, "UPDATE meja SET qrcode='$db_path' WHERE id_meja=$id_meja");
+
+    echo "QR Code untuk meja $id_meja dibuat: <img src='$db_path'><br>";
 }
+?>
